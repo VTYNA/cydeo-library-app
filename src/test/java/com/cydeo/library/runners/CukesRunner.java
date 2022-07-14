43fd8f0eb0dfab7cfc97bd694834a5a848420be2
vlentinajakuts <y?e3,L3~k4:-Fe>
2022-07-13 20:50:21 -0500

    cuekesrunner

--- src/test/java/com/cydeo/library/runners/CukesRunner.java
+++ src/test/java/com/cydeo/library/runners/CukesRunner.java
@@ -6,6 +6,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 
     @CucumberOptions(
+            plugin = {
+                    "json:target/cucumber.json",
+                    "html:target/cucumber/report.html",
+                    "junit:target/junit/junit-report.xml",
+                    "rerun:target/rerun.txt",},
                     features="src/test/resources/features",//business layer
                     glue="com/cydeo/library/step_definitions",///implementation layer -> stands for step definition package
                     dryRun = false,////false run the test, true -> checks for missing steps
